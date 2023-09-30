e1d1dbe9f5e0289d97944e8c8e8e6a295d1c3653
Ripcv <gefallene.engel@gmail.com>
2023-09-29 22:21:06 -0300

    Se crean mocks y se importan en index

---
+++ mocks/clientes.js
@@ -0,0 +1,26 @@
+let clientesMocks = [
+        {
+        nombre: "Raul",
+        apellido: "Alvarez",
+        rut: "12345678-9",
+        fechaNacimiento: "2023/06/26",
+        correo: "raul@alvarez.cl",
+        clave: "123456789",
+        },
+        {
+        nombre: "Luis",
+        apellido: "González",
+        rut: "98765432-1",
+        fechaNacimiento: "1990/03/15",
+        correo: "luis@gmail.com",
+        clave: "0123456789",
+        },
+        {
+        nombre: "Ana",
+        apellido: "Martínez",
+        rut: "56789012-3",
+        fechaNacimiento: "1985/09/10",
+        correo: "ana@martinez.com",
+        clave: "password",
+        },
+]
---
+++ mocks/ordenes.js
@@ -0,0 +1,51 @@
+let ordenesMocks = [
+    {
+        rutCliente: "12345678-9",
+        idOrden: 115,
+        productos: [{
+            id: 9,
+            valor: 20000,
+            cantidad: 2
+        },
+        {
+            id: 5,
+            valor: 25000,
+            cantidad: 1
+        }
+        ],
+        fechaCompra: "2023/09/21",
+        fechaEnvio: "2023/09/25",
+        totalCompra: 45000,
+    },
+    {
+        rutCliente: "12345678-9",
+        idOrden: 136,
+        productos: [{
+            id: 12,
+            valor: 35000,
+            cantidad: 1
+        }
+        ],
+        fechaCompra: "2023/09/24",
+        fechaEnvio: "2023/09/27",
+        totalCompra: 35000,
+    },
+    {
+        rutCliente: "12345678-9",
+        idOrden: 145,
+        productos: [{
+            id: 2,
+            valor: 10000,
+            cantidad: 1
+        },
+        {
+            id: 4,
+            valor: 15000,
+            cantidad: 3
+        }
+        ],
+        fechaCompra: "2023/09/25",
+        fechaEnvio: "2023/09/29",
+        totalCompra: 55000,
+    },
+]
